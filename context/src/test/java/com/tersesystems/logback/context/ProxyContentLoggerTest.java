0212296260a08f4ecf1ed79619474a3376b702fe
Will Sargent <will@tersesystems.com>
2019-02-06 09:10:21 -0800

    context test works

--- context/src/test/java/com/tersesystems/logback/context/ProxyContentLoggerTest.java
+++ context/src/test/java/com/tersesystems/logback/context/ProxyContentLoggerTest.java
@@ -1,30 +1,53 @@
 package com.tersesystems.logback.context;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 import net.logstash.logback.marker.LogstashMarker;
+import net.logstash.logback.marker.Markers;
+import org.assertj.core.groups.Tuple;
 import org.junit.Test;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProxyContentLoggerTest {
 
+    static class Foo {
+        private final org.slf4j.Logger logger;
+
+        Foo(org.slf4j.Logger logger) {
+            this.logger = logger;
+        }
+
+        public void doThat() {
+            logger.info("hello world");
+        }
+    }
+
     @Test
     public void testContext() throws Exception {
-        LoggerContext loggerContext = new LoggerContext();
-        ListAppender<LoggingEvent> listAppender = new ListAppender<>();
-        listAppender.setContext(loggerContext);
+        // create and start a ListAppender
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
         listAppender.start();
-        loggerContext.start();
 
-        Context<LogstashMarker> context = LogstashContext.create();
-        ILoggerFactory proxyLogger = ProxyContextLoggerFactory.create(context, loggerContext);
-        Logger logger = proxyLogger.getLogger("example.Class");
-        logger.info("hello world!");
+        Context<LogstashMarker> context = LogstashContext.create("key1", "value1");
+        ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        ch.qos.logback.classic.Logger underlyingLogger = (ch.qos.logback.classic.Logger) loggerFactory.getLogger(Foo.class.getName());
+        // add the appender to the logger
+        underlyingLogger.addAppender(listAppender);
+        Logger logger = new ProxyContextLogger<>(context, underlyingLogger);
 
-        assertThat(listAppender.list).isNotEmpty();
+        // call method under test
+        Foo foo = new Foo(logger);
+        foo.doThat();
+
+        LogstashMarker contextMarker = Markers.appendEntries(Collections.singletonMap("key1", "value1"));
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getMessage, ILoggingEvent::getMarker)
+                .containsExactly(Tuple.tuple("hello world", contextMarker));
     }
 }
