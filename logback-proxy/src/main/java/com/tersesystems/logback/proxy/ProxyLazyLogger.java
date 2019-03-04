78e524ab453f34ad526fb914629a674c045738e4
Will Sargent <will@tersesystems.com>
2019-03-03 20:22:55 -0800

    fix copyright

--- logback-proxy/src/main/java/com/tersesystems/logback/proxy/ProxyLazyLogger.java
+++ logback-proxy/src/main/java/com/tersesystems/logback/proxy/ProxyLazyLogger.java
@@ -3,10 +3,10 @@
  *
  * Copyright 2018-2019 Will Sargent.
  *
- * Licensed under the CC0 default Domain Dedication;
+ * Licensed under the CC0 Public Domain Dedication;
  * You may obtain a copy of the License at
  *
- *     http://creativecommons.org/defaultdomain/zero/1.0/
+ *     http://creativecommons.org/publicdomain/zero/1.0/
  */
 package com.tersesystems.logback.proxy;
 
