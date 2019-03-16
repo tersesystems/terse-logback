f4d03d646f0c9069fc1a590610ad4a40a94d790c
Will Sargent <will.sargent@gmail.com>
2019-03-16 09:49:43 -0700

    Add tests (#23)

--- logback-core/src/test/java/com/tersesystems/logback/README.md
+++
@@ -1,5 +0,0 @@
-# Java Testing
-
-Use a ListAppender?
-
-https://stackoverflow.com/questions/29076981/how-to-intercept-slf4j-logging-via-a-junit-test
---
+++ logback-core/src/test/java/com/tersesystems/logback/TypesafeConfigActionTest.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  * SPDX-License-Identifier: CC0-1.0
+ *  *
+ *  * Copyright 2018-2019 Will Sargent.
+ *  *
+ *  * Licensed under the CC0 Public Domain Dedication;
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ */
+
+package com.tersesystems.logback;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.Context;
+import com.typesafe.config.Config;
+import org.junit.Test;
+
+import static com.tersesystems.logback.TypesafeConfigAction.LOGBACK_DEBUG_PROPERTY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TypesafeConfigActionTest {
+
+    @Test
+    public void testConfigAction() {
+        TypesafeConfigAction action = new TypesafeConfigAction();
+
+        System.setProperty(LOGBACK_DEBUG_PROPERTY, "true");
+        Context lc = new LoggerContext();
+        Config config = action.generateConfig(this.getClass().getClassLoader());
+        action.configureContextWithConfig(lc, config);
+
+        String fooValue = lc.getProperty("foo");
+        assertThat(fooValue).isEqualTo("bar");
+    }
+}
