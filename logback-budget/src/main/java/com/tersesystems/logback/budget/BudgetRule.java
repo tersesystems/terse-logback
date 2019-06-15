ced60b973fbfc424fad015d734baef61817cb855
Will Sargent <will@tersesystems.com>
2019-06-15 13:38:40 -0700

    Update budget based evaluator

---
+++ logback-budget/src/main/java/com/tersesystems/logback/budget/BudgetRule.java
@@ -0,0 +1,61 @@
+/*
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Copyright 2018-2019 Will Sargent.
+ *
+ * Licensed under the CC0 Public Domain Dedication;
+ * You may obtain a copy of the License at
+ *
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package com.tersesystems.logback.budget;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class BudgetRule {
+
+    private String name;
+    private int threshold;
+    private long interval;
+    private String timeUnit;
+
+    public BudgetRule() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public long getInterval() {
+        return interval;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    public String getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(String timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
---
+++ logback-budget/src/main/java/com/tersesystems/logback/budget/BudgetRuleAttachable.java
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Copyright 2018-2019 Will Sargent.
+ *
+ * Licensed under the CC0 Public Domain Dedication;
+ * You may obtain a copy of the License at
+ *
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package com.tersesystems.logback.budget;
+
+public interface BudgetRuleAttachable {
+
+    void addBudgetRule(BudgetRule budget);
+
+    void clearAllBudgetRules();
+}
