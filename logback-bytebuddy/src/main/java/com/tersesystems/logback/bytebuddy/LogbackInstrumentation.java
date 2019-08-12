6f67961de241b341e1c79a2c15eb86531c0b31dc
Will Sargent <will.sargent@gmail.com>
2019-08-12 09:15:49 -0700

    Update with licenses

--- logback-bytebuddy/src/main/java/com/tersesystems/logback/bytebuddy/LogbackInstrumentation.java
+++ logback-bytebuddy/src/main/java/com/tersesystems/logback/bytebuddy/LogbackInstrumentation.java
@@ -1,3 +1,13 @@
+/*
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Copyright 2018-2019 Will Sargent.
+ *
+ * Licensed under the CC0 Public Domain Dedication;
+ * You may obtain a copy of the License at
+ *
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
 package com.tersesystems.logback.bytebuddy;
 
 import com.typesafe.config.Config;
