9d87d3fd6ee8e682a727cb8a9c95792f22d0d74c
Will Sargent <will@tersesystems.com>
2020-03-07 13:23:20 -0800

    Add uniqueid field to correlation id (#111)
    * Add uniqueid field to correlation id
    
    * Format

--- logback-correlationid/src/main/java/com/tersesystems/logback/correlationid/CorrelationIdJDBCAppender.java
+++ logback-correlationid/src/main/java/com/tersesystems/logback/correlationid/CorrelationIdJDBCAppender.java
@@ -2,13 +2,14 @@ package com.tersesystems.logback.correlationid;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.tersesystems.logback.jdbc.JDBCAppender;
+import com.tersesystems.logback.uniqueid.IUniqueIdLoggingEvent;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Optional;
 import java.util.concurrent.atomic.LongAdder;
 
-/** Writes out log entry with a correlation id explicitly. */
+/** Writes out log entry with a correlation id and event id explicitly. */
 public class CorrelationIdJDBCAppender extends JDBCAppender {
   private String mdcKey = "correlation_id";
 
@@ -32,6 +33,18 @@ public class CorrelationIdJDBCAppender extends JDBCAppender {
   protected void insertAdditionalData(
       ILoggingEvent event, LongAdder adder, PreparedStatement statement) throws SQLException {
     insertCorrelationId(event, adder, statement);
+    insertEventId(event, adder, statement);
+  }
+
+  private void insertEventId(ILoggingEvent event, LongAdder adder, PreparedStatement statement)
+      throws SQLException {
+    if (event instanceof IUniqueIdLoggingEvent) {
+      IUniqueIdLoggingEvent eventWithId = (IUniqueIdLoggingEvent) event;
+      statement.setString(adder.intValue(), eventWithId.uniqueId());
+    } else {
+      statement.setNull(adder.intValue(), Types.VARCHAR);
+    }
+    adder.increment();
   }
 
   private void insertCorrelationId(
