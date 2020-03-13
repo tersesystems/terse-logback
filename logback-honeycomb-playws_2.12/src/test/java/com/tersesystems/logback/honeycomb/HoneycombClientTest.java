76ae015aa5cbc502f8455e2955c51a5caa07dcb0
Will Sargent <will@tersesystems.com>
2020-03-13 14:46:49 -0700

    Add copyright to everything. (#118)

--- logback-honeycomb-playws_2.12/src/test/java/com/tersesystems/logback/honeycomb/HoneycombClientTest.java
+++ logback-honeycomb-playws_2.12/src/test/java/com/tersesystems/logback/honeycomb/HoneycombClientTest.java
@@ -1,12 +1,12 @@
 /*
  * SPDX-License-Identifier: CC0-1.0
  *
- * Copyright 2018-2019 Will Sargent.
+ * Copyright 2018-2020 Will Sargent.
  *
  * Licensed under the CC0 Public Domain Dedication;
  * You may obtain a copy of the License at
  *
- *     http://creativecommons.org/publicdomain/zero/1.0/
+ *  http://creativecommons.org/publicdomain/zero/1.0/
  */
 package com.tersesystems.logback.honeycomb;
 
